a4c7b0d2ea59632f545c67ff8a763bf104ccdeba
Tristan Alexander Ingold <tingold@uw.edu>
2018-05-18 15:15:46 -0700

    Add Shiny Application Structure

--- ui.R
+++ ui.R
@@ -4,8 +4,70 @@
 library(shiny)
 library(plotly)
 library(shinythemes)
+library(dplyr)        
 
 #---- Start UI
-shinyUI(fluidPage(
-
-  ))
+shinyUI(navbarPage(
+  theme = shinytheme("slate"), 
+  "Kickstarter",
+    tabPanel(
+      "Home",
+      tags$h1("What is Kickstater?"),
+      sidebarLayout(
+        sidebarPanel(
+          tags$h2("Second Header")
+          ), 
+        mainPanel(
+          plotOutput("plot")
+        )
+        )
+    ),
+    tabPanel(
+      "Tab 2",
+      tags$h1("What is Kickstater?"),
+      sidebarLayout(
+        sidebarPanel(
+          tags$h2("Second Header")
+        ), 
+        mainPanel(
+          plotOutput("plot")
+        )
+      )
+    ),
+    tabPanel(
+      "Tab 3",
+      tags$h1("What is Kickstater?"),
+      sidebarLayout(
+        sidebarPanel(
+          tags$h2("Second Header")
+        ), 
+        mainPanel(
+          plotOutput("plot")
+        )
+      )
+    ),
+    tabPanel(
+      "Tab 4",
+      tags$h1("What is Kickstater?"),
+      sidebarLayout(
+        sidebarPanel(
+          tags$h2("Second Header")
+        ), 
+        mainPanel(
+          plotOutput("plot")
+        )
+      )
+    ),
+    tabPanel(
+      "Tab 5",
+      tags$h1("What is Kickstater?"),
+      sidebarLayout(
+        sidebarPanel(
+          tags$h2("Second Header")
+        ), 
+        mainPanel(
+          plotOutput("plot")
+        )
+      )
+    )
+))
