3f06487a04d9817cabf63d3cede14828e976af20
Maegan Nevalsky <maeagann@uw.edu>
2018-05-30 12:42:16 -0700

    update goal plot explanation

--- ui.R
+++ ui.R
@@ -159,15 +159,18 @@ shinyUI(navbarPage(
         ), 
         mainPanel(
           plotOutput("plot_money"),
-          tags$p("This plot shows trends between the target fundraising amount of the project in
-                 comparison to the actual amount raised. The plots can be adjusted based on the 
-                 min and max amounts for either axis in order to see trends for projects in a 
-                 similar price range to your potential project."),
-          tags$p("The projects that lie along the bottom of the graph are likely the ones that
-                 did not succeed because they did not meet their fundraising goal. In most 
-                 categories, there is a trendline at a slope of 1 because many projects raise just 
-                 over their goal. As the goal amount gets higher, there are not many projects that 
-                 fall just short of the goal, most of them either reach the goal or don't come close.")
+          tags$p("This plot shows trends between the target fundraising amount 
+                 of the project incomparison to the actual amount raised. The 
+                 plots can be adjusted based on the min and max amounts for 
+                 either axis in order to see trends for projects in a similar 
+                 price range to your potential project."),
+          tags$p("The projects that lie along the bottom of the graph are likely 
+                 the ones that did not succeed because they did not meet their 
+                 fundraising goal. In most categories, there is a trendline at a 
+                 slope of 1 because many projects raise just over their goal. As 
+                 the goal amount gets higher, there are not many projects that 
+                 fall just short of the goal, most of them either reach the goal 
+                 or don't come close.")
         )
       )
     ),
